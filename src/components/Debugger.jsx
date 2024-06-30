9be8684d925aceaa3ff324087a3a69baf8bfc132
Alessandro Daniele <alessandrodaniele07@gmail.com>
2024-06-29 23:30:35 -0500

    feat: add syntax highlighting to debugger code lines

--- src/components/Debugger.jsx
+++ src/components/Debugger.jsx
@@ -1,23 +1,38 @@
-import { Box, Code, IconButton, Tooltip } from "@chakra-ui/react";
+import {
+  Box,
+  Code,
+  IconButton,
+  Tooltip,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from "@chakra-ui/react";
 import { ArrowForwardIcon, CloseIcon, ArrowDownIcon } from "@chakra-ui/icons";
 import { useState, useRef, useEffect } from "react";
-import { codeLines } from "../algorithms/codeLines"
+import { codeLines } from "../algorithms/codeLines";
 
 const Debugger = ({ lines, onExecutionFinished }) => {
   const codeColor = "#F2F2EF";
   const highlightColor = "blue";
-  const [currentLine, setCurrentLine] = useState(lines[0]-1);
+  const [currentLine, setCurrentLine] = useState(lines[0] - 1);
+  const [selectedLine, setSelectedLine] = useState(null);
+  const [noSelectLine, setNoSelectLine] = useState(false);
   const indice = useRef(1);
   const lineRefs = useRef([]);
+  const [showAlert, setShowAlert] = useState(false); 
 
   useEffect(() => {
     if (lineRefs.current[currentLine]) {
-      lineRefs.current[currentLine].scrollIntoView({ behavior: 'smooth', block: 'center' });
+      lineRefs.current[currentLine].scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
     }
   }, [currentLine]);
 
-
   const handleContinue = () => {
+    setShowAlert(false);
     if (indice.current < lines.length) {
       setCurrentLine(lines[indice.current] - 1);
       indice.current++;
@@ -33,40 +48,60 @@ const Debugger = ({ lines, onExecutionFinished }) => {
     setCurrentLine(null);
     indice.current = 1;
     onExecutionFinished(false);
+    setSelectedLine(null);
   };
 
   const handleJump = () => {
+    //console.log("select line", selectedLine);
+    let found = false;
     for (let i = indice.current; i < lines.length; i++) {
-      if (lines[i] == 5 && lines[i + 1] == 6) {
-        console.log("salta");
-        indice.current = i + 1;
-        setCurrentLine(i + 1);
-        handleContinue();
+      if (lines[i] - 1 === selectedLine) {
+        setCurrentLine(selectedLine);
+        indice.current = i+1;
+        found = true;
+        //console.log("la current line es ", currentLine);
         break;
       }
     }
+    if (!found) {
+      setShowAlert(true); 
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 10000); 
+    } else {
+      setShowAlert(false); // Ocultar alerta si se encontró la línea seleccionada
+    }
+  };
+
+  const handleLineClick = (index) => {
+    !noSelectLine ? setSelectedLine(index) : setSelectedLine(null);
+    setNoSelectLine(!noSelectLine);
+    console.log("entra", ++index);
   };
 
   return (
     <Box display="flex" justifyContent="flex-end" flexDir="column-reverse">
       <Box justifyContent="center" m="auto">
-      <Tooltip label="Step-by-Step Execution" aria-label="Step-by-Step  Execution">
-        <IconButton
-          colorScheme="green"
-          size="sm"
-          aria-label="Step"
-          icon={<ArrowForwardIcon />}
-          m={2}
-          onClick={handleContinue}
-        />
+        <Tooltip
+          label="Step-by-Step Execution"
+          aria-label="Step-by-Step  Execution"
+        >
+          <IconButton
+            colorScheme="green"
+            size="sm"
+            aria-label="Step"
+            icon={<ArrowForwardIcon />}
+            m={2}
+            onClick={handleContinue}
+          />
         </Tooltip>
         <Tooltip label="Continue Execution" aria-label="Continue Execution">
-        <IconButton
-          colorScheme="blue"
-          size="sm"
-          icon={<ArrowDownIcon />}
-          onClick={handleJump}
-        />
+          <IconButton
+            colorScheme="blue"
+            size="sm"
+            icon={<ArrowDownIcon />}
+            onClick={handleJump}
+          />
         </Tooltip>
         <Tooltip label="Stop Execution" aria-label="Stop Execution">
           <IconButton
@@ -79,25 +114,54 @@ const Debugger = ({ lines, onExecutionFinished }) => {
         </Tooltip>
       </Box>
 
-      <Box borderRadius={9} w="100%" p={2} maxH="50vh" bg={codeColor} overflow="auto">
-      {codeLines.map((line, index) => (
-          <Box display="flex" key={index} ref={(el) => (lineRefs.current[index] = el)}>
+      {showAlert && ( 
+        <Alert status="info">
+          <AlertIcon />
+          <AlertTitle>Execution Information</AlertTitle>
+          <AlertDescription>
+          The code execution does not pass through this line or you have selected a line that the debugger has already passed. Please try another.
+          </AlertDescription>
+        </Alert>
+      )}
+      <Box
+        borderRadius={9}
+        w="100%"
+        p={2}
+        maxH="50vh"
+        bg={codeColor}
+        overflow="auto"
+      >
+        {codeLines.map((line, index) => (
+          <Box
+            key={index}
+            display="flex"
+            onClick={() => handleLineClick(index)}
+            ref={(el) => (lineRefs.current[index] = el)}
+            bg={selectedLine === index ? "tomato" : "transparent"}
+            borderRadius="sm"
+          >
             <Box
               width="30px"
               textAlign="right"
               pr={2}
               userSelect="none"
-              color={currentLine === index ? highlightColor : 'gray'}
+              color={currentLine === index ? highlightColor : "gray"}
+              cursor="pointer"
             >
               {index + 1}
             </Box>
             <Code
               whiteSpace="pre"
-              colorScheme={currentLine === index ? highlightColor : codeColor}
               flex="1"
-            >
-              {line}
-            </Code>
+              colorScheme={currentLine === index ? highlightColor :  codeColor}
+              style={{
+                overflowWrap: "break-word",
+                wordWrap: "break-word",
+                wordBreak: "break-word",
+                display: "inline-block",
+              }}
+              dangerouslySetInnerHTML={{ __html: line }}
+            />
           </Box>
         ))}
       </Box>
