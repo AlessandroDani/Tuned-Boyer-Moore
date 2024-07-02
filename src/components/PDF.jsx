f0add5c2ebd85fdd345ed813145951bc4cb822a4
Juan1806y <juandavidorca@ufps.edu.co>
2024-07-01 19:45:34 -0500

    fix:PDF name

--- src/components/PDF.jsx
+++ src/components/PDF.jsx
@@ -16,13 +16,13 @@ class PDF extends Component {
         return (
             <div style={{position: 'flex', width: '100%', height: '100vh'}}>
                 <object
-                data='/a.pdf'
+                data='/Costo Algoritmico 1152298 - 1152194.pdf'
                 type="application/pdf"
                 width="100%"
                 height="100%"
                 >
                     <br />
-                    <a href='/a.pdf' id="enlaceDescargarPdf"
+                    <a href='/Costo Algoritmico 1152298 - 1152194.pdf' id="enlaceDescargarPdf"
                     download="ReactJS.pdf"
                     >Tu dispositivo no puede visualizar los PDF, da click aquí para descargarlo</a>
                 </object>
